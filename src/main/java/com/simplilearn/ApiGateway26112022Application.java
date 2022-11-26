1fd308e57c5af8b3014bf053cf8d0376dec59b03
Dhruvik <dhruviksparikh@gmail.com>
2022-11-26 22:57:29 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/ApiGateway26112022Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateway26112022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateway26112022Application.class, args);
+	}
+
+}
